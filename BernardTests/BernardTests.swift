802b6928fd48d06ede95e8377f88c5d5aa657c20
Michael Johnson <mj@notthatmj.com>
2016-12-05 17:17:53 -0600

    Add button

--- BernardTests/BernardTests.swift
+++ BernardTests/BernardTests.swift
@@ -15,22 +15,17 @@ class BernardTests: XCTestCase {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    func loadView(for viewController : UIViewController?) {
+        _ = viewController?.view
     }
     
-    func testExample() {
+    func testMainStoryboard() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let storyboard = UIStoryboard.init(name: "Main", bundle: Bundle.main)
+        let viewController = storyboard.instantiateInitialViewController() as? ViewController
+        self.loadView(for: viewController)
+        XCTAssertNotNil(viewController?.button);
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
