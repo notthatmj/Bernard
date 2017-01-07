ed5a046d8934940d5660dcbdfc612b5baede2dca
Michael Johnson <mj@notthatmj.com>
2017-01-07 15:03:25 -0600

    Add second scene to tab bar

--- BernardTests/TabBarControllerTests.swift
+++ BernardTests/TabBarControllerTests.swift
@@ -22,7 +22,7 @@ class TabBarControllerTests: XCTestCase {
         XCTAssertNotNil(SUT)
         XCTAssertNotNil(SUT?.viewControllers)
         if let viewControllers = SUT?.viewControllers {
-            XCTAssertEqual(viewControllers.count, 1)
+            XCTAssertEqual(viewControllers.count, 2)
             XCTAssertNotNil(viewControllers[0] as? ViewController)
             
         }
