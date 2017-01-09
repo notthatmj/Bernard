632dfe73b40f77c32c92d2a36f6a6a0ce0025070
Michael Johnson <mj@notthatmj.com>
2017-01-09 15:23:28 -0600

    Add class

---
+++ Bernard/FavoritesDataSource.swift
@@ -0,0 +1,11 @@
+//
+//  FavoritesDataSource.swift
+//  Bernard
+//
+//  Created by Michael Johnson on 1/9/17.
+//  Copyright © 2017 Michael Johnson. All rights reserved.
+//
+
+import Foundation
+
+class FavoritesDataSource { }
---
+++ BernardTests/FavoritesDataSourceTests.swift
@@ -0,0 +1,23 @@
+//
+//  FavoritesDataSource.swift
+//  Bernard
+//
+//  Created by Michael Johnson on 1/9/17.
+//  Copyright © 2017 Michael Johnson. All rights reserved.
+//
+
+import XCTest
+@testable import Bernard
+
+class FavoritesDataSourceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    func testFavoritesDataSource() {
+        _ = FavoritesDataSource()
+    }
+    
+}
