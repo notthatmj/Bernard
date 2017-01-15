93123a096ed522b1adf6931bc0a969a48ee5ebc8
Michael Johnson <mj@notthatmj.com>
2017-01-15 14:00:31 -0600

    Remove empty method

--- Bernard/FavoritesTableViewController.swift
+++ Bernard/FavoritesTableViewController.swift
@@ -28,10 +28,6 @@ class FavoritesTableViewController: UITableViewController, NamesModelObserving {
         namesModel?.addObserver(self)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
